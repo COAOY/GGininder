f009d8644bd1fea91f89a5a3682c9b01b9f3978f
joe <xine00021@gmail.com>
2019-05-08 08:51:51 -0700

    set env

---
+++ ba.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import seaborn; seaborn.set_style('whitegrid')
+import numpy
+
+from pomegranate import *
+
+numpy.random.seed(0)
+numpy.set_printoptions(suppress=True)
