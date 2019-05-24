f13c32c3862c373c3948b11d88bb3962b650b6a1
鄭景鴻 <zhengjinghong@zhengjinghongde-MacBook-Pro.local>
2019-05-24 11:30:24 +0800

    #1 class gate body fit

--- cpdag_atpg.py
+++ cpdag_atpg.py
@@ -28,43 +28,58 @@ class CPD:
 
 	def parsar(self):
 		'''
-		only implement nand,and,or gate and input
+		parasar file
+
 		'''
 		num=0
 		infile=open(self.filename,'r')
-		gate_list=[]
+		line_list=[]
 		for line in infile:
-			gate_list.append(line)
+			line_list.append(line)
+		close(infile)
+
+		for line in line_list:
+			
 
 
-		for line in gate_list:
-			if(line[0]== 'i'):
-				inp=line.split()
-				w1 = Gate(deepcopy(line[0]),deepcopy(inp),self.model,deepcopy(num), self.Gate_dict )
-				self.wire.append(w1)
-				self.Gate_dict.setdefault(w1.return_name(),w1)
-				num=num+1
-				# print(inp[1])
-			elif(line[0]== 'o'):
-				continue
-				# inp=line.split()
-				# outwire.append(deepcopy(inp) )
-				# outnum.append(deepcopy(num))
-				# print(inp[1])
-			elif(line[0]== 'g'):
-				inp=line.split()
-				w1 = Gate(deepcopy(line[0]),deepcopy(inp[1:]),self.model,deepcopy(num),self.Gate_dict )
-				self.wire.append(w1)
-				print(w1.return_name())
-				self.Gate_dict.setdefault(w1.return_name(),w1)
-				num=num+1
-				# print(inp[5])
-			else:
-				# print(" input file have ilegal parameter")
-				pass
-		for k, v in self.Gate_dict.items():
-			v.build_conditional_prob()
-		
+
+		# for line in gate_list:
+		# 	if(line[0]== 'i'):
+		# 		inp=line.split()
+		# 		w1 = Gate(deepcopy(line[0]),deepcopy(inp),self.model,deepcopy(num), self.Gate_dict )
+		# 		self.wire.append(w1)
+		# 		self.Gate_dict.setdefault(w1.return_name(),w1)
+		# 		num=num+1
+		# 		# print(inp[1])
+		# 	elif(line[0]== 'o'):
+		# 		continue
+		# 		# inp=line.split()
+		# 		# outwire.append(deepcopy(inp) )
+		# 		# outnum.append(deepcopy(num))
+		# 		# print(inp[1])
+		# 	elif(line[0]== 'g'):
+		# 		inp=line.split()
+		# 		w1 = Gate(deepcopy(line[0]),deepcopy(inp[1:]),self.model,deepcopy(num),self.Gate_dict )
+		# 		self.wire.append(w1)
+		# 		print(w1.return_name())
+		# 		self.Gate_dict.setdefault(w1.return_name(),w1)
+		# 		num=num+1
+		# 		# print(inp[5])
+		# 	else:
+		# 		# print(" input file have ilegal parameter")
+		# 		pass
+		# for k, v in self.Gate_dict.items():
+		# 	v.build_conditional_prob()
+
+	def logic_simulation(self):
+		pass
+
+
+
+
+
+
+
 	def return_bayRef(self):
 		return self.model
 #
--- gate.py
+++ gate.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn; seaborn.set_style('whitegrid')
 import numpy
 import copy
+import itertools
 from pomegranate import *
 
 
@@ -13,95 +14,72 @@ class Gate:
 	bay_ref = None
 	name = None
 	gid = None
+	gtype = None
 	gcref = None
 	gsref = None
+	rely_list = []
 	'''
-	arrtibute(string): the type of gate, input(i), ouput(o), gate(g)
+	arrtibute(string): the type of gate, input(i), gate(g)
 	sequence(string) : read line from file
 	bay_ref(ref)	 : baysian reference
 	name(string): the name of gate
 	gid(int) : gate id in the bay list
-	gcref(ref) = gate condition refference
-	gsref(ref) = gate state refference
+	gtype(string): type of gate ex nand, and, or ~
+	gcref(ref): gate condition refference
+	gsref(ref): gate state refference
+	rely_list[list]: the gate input in this gate 
 	'''
 	def __init__(self, arrtibute, sequence, bay_ref, gid, gate_dic):
 		self.arrt = arrtibute
 		self.seq = sequence
 		self.bay_ref = bay_ref
 		self.gid = gid
-		self.gate_dic=gate_dic
-		if self.arrt == 'i':
-			# arrtibute: input
-			self.name = self.seq[1]
-			self.add_Input_to_bayesian()
-		elif self.arrt == 'g':
-			# arrtibute: gate
-			if self.seq[0]=='nand':
-				if len(self.seq)==5:
-					self.name = self.seq[4]
-				elif len(self.seq)==4:
-					self.name = self.seq[3]
-				else:
-					print(" input file have ilegal NAND gate")
-			elif self.seq[0]=='and':
-				if len(self.seq)==5:
-					self.name = self.seq[4]
-				elif len(self.seq)==4:
-					self.name = self.seq[3]
-				else:
-					print(" input file have ilegal AND gate")
-			elif self.seq[0]=='or':
-				if len(self.seq)==5:
-					self.name = self.seq[4]
-				elif len(self.seq)==4:
-					self.name = self.seq[3]
-				else:
-					print(" input file have ilegal OR gate")
-			elif self.seq[0]=='not':
-				self.name = self.seq[3]
-			else:
-				print(" ilegal gate!!")
+		self.gate_dic = gate_dic
+		#set gtype
+		# set input
+		if(self.arrt=='g'):
+			self.gtype = self.seq[1]
+			for i in range(2:len(self.seq)):
+				self.rely_list.append(self.seq[i])
+
+		# set name
+		for i in self.seq:
+			if i == ';':
+				self.name = self.seq[i+1]
+				break
 		
 		
-	def build_conditional_prob(self):
-		if self.arrt == 'i':
-			# arrtibute: input
-			self.name = self.seq[1]
+		
+
+	def bay_conditional_prob_buid(self):
+		'''
+		plz use this fun to create abyesian
+		'''
+		if(self.arrt=='i'):
 			self.add_Input_to_bayesian()
-		elif self.arrt == 'g':
-			# arrtibute: gate
-			if self.seq[0]=='nand':
-				if len(self.seq)==5:
-					self.name = self.seq[4]
-					self.add_Nand_to_bayesian(self.seq[1],self.seq[2])
-				elif len(self.seq)==4:
-					self.name = self.seq[3]
-					self.add_Nand_to_bayesian(self.seq[1],self.seq[1])
-				else:
-					print(" input file have ilegal NAND gate")
-			elif self.seq[0]=='and':
-				if len(self.seq)==5:
-					self.name = self.seq[4]
-					self.add_And_to_bayesian(self.seq[1],self.seq[2])
-				elif len(self.seq)==4:
-					self.name = self.seq[3]
-					self.add_And_to_bayesian(self.seq[1],self.seq[1])
-				else:
-					print(" input file have ilegal AND gate")
-			elif self.seq[0]=='or':
-				if len(self.seq)==5:
-					self.name = self.seq[4]
-					self.add_Or_to_bayesian(self.seq[1],self.seq[2])
-				elif len(self.seq)==4:
-					self.name = self.seq[3]
-					self.add_Or_to_bayesian(self.seq[1],self.seq[1])
-				else:
-					print(" input file have ilegal OR gate")
-			elif self.seq[0]=='not':
-				self.name = self.seq[3]
-				self.add_Not_to_bayesian(self.seq[1])
-			else:
-				print(" ilegal gate!!")
+		else:
+			self.build_gate_conditional_prob()
+
+	def build_gate_conditional_prob(self):
+		'''
+			buid baye condition except input
+			plz do not use input in this fun.
+		'''
+		number_of_input = self.rely_list
+		table self.assign_ouput_to_true_tble(self.build_true_table(number_of_input),self.gtype)
+		# find others gcref ref and create list
+		glist=[]
+		for i in self.rely_list:
+			glist.append(self.gate_dic[i].return_gcref())
+		if len(glist)==1 and gtype != 'not':
+			glist.append(glist[0])
+
+		self.gcref =ConditionalProbabilityTable(table,glist)
+		self.gsref = State(self.gcref, name=self.name)
+		#create edge
+		for i in len(glist):
+			self.bay_ref.add_edge(i,self.gsref)
+
 
 	def return_bayref(self):
 		return self.bay_ref
@@ -110,76 +88,9 @@ class Gate:
 		self.gcref = DiscreteDistribution({True: 0.5, False: 0.5})
 		self.gsref = State(self.gcref, name=self.name)
 		self.bay_ref.add_states(self.gsref)
+		
+
 	
-
-	def add_Nand_to_bayesian(self, d1, d2):
-
-		self.gcref =ConditionalProbabilityTable(
-        [[True,  True,  True,  0.0],
-         [True,  True,  False, 1.0],
-         [True,  False, True,  1.0],
-         [True,  False, False, 0.0],
-         [False, True,  True,  1.0],
-         [False, True,  False, 0.0],
-         [False, False, True,  1.0],
-         [False, False, False, 0.0]], [self.gate_dic[d1].return_gcref(), self.gate_dic[d2].return_gcref()])
-		self.gsref = State(self.gcref, name=self.name)
-		self.bay_ref.add_states(self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d1].return_gsref(),self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d2].return_gsref(),self.gsref)
-		
-
-
-	def add_And_to_bayesian(self, d1, d2):
-
-		self.gcref =ConditionalProbabilityTable(
-        [[True,  True,  True,  1.0],
-         [True,  True,  False, 0.0],
-         [True,  False, True,  0.0],
-         [True,  False, False, 1.0],
-         [False, True,  True,  1.0],
-         [False, True,  False, 0.0],
-         [False, False, True,  0.0],
-         [False, False, False, 1.0]], [self.gate_dic[d1].return_gcref(), self.gate_dic[d2].return_gcref()])
-		self.gsref = State(self.gcref, name=self.name)
-		self.bay_ref.add_states(self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d1].return_gsref(),self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d2].return_gsref(),self.gsref)
-		
-
-
-
-	def add_Or_to_bayesian(self, d1, d2):
-
-		self.gcref =ConditionalProbabilityTable(
-        [[True,  True,  True,  1.0],
-         [True,  True,  False, 0.0],
-         [True,  False, True,  1.0],
-         [True,  False, False, 0.0],
-         [False, True,  True,  1.0],
-         [False, True,  False, 0.0],
-         [False, False, True,  0.0],
-         [False, False, False, 1.0]], [self.gate_dic[d1].return_gcref(), self.gate_dic[d2].return_gcref()])
-		self.gsref = State(self.gcref, name=self.name)
-		self.bay_ref.add_states(self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d1].return_gsref(),self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d2].return_gsref(),self.gsref)
-		
-
-
-
-	def add_Not_to_bayesian(self, d1):
-
-		self.gcref =ConditionalProbabilityTable(
-        [[True,  True,  0.0],
-         [True,  False, 1.0],
-         [False, True, 1.0],
-         [False,  False, 0.0],], [self.gate_dic[d1].return_gcref()])
-		self.gsref = State(self.gcref, name=self.name)
-		self.bay_ref.add_states(self.gsref)
-		self.bay_ref.add_edge(self.gate_dic[d1].return_gsref(),self.gsref)
-		
-
 
 	def return_name(self):
 		return self.name
@@ -187,3 +98,58 @@ class Gate:
 		return self.gcref
 	def return_gsref(self):
 		return self.gsref
+
+
+	def build_true_table(self, input_number):
+		'''
+		input: 
+		[int] input_number: # of gate port 
+		return:
+		[list] true table 
+		'''
+		lis=[]
+		for i in itertools.product([True,False],repeat=input_number+1):
+			list_table_seq =list(i)
+			list_table_seq.append(0.0)
+			lis.append(list_table_seq)
+
+		return lis
+
+	def assign_ouput_to_true_tble(self, table, gtype):
+		'''
+		!!!!! can not process input wire !!!!!
+		input
+		[list]table: true table
+		[string] gtype: gate type
+		return 
+		[list]table: re assign gate output
+		'''
+		for j in table:
+			res=j[0]
+			for t in range(1,len(j)-2):
+				if(gtype=='and'):
+					res = res&j[t]
+				elif(gtype=='nand'):
+					res = res&j[t]
+				elif(gtype=='or'):
+					res = res|j[t]
+				elif(gtype=='xor'):
+					res = res^j[t]
+				elif(gtype=='not'):
+					res = not res
+				else:
+					print('wronf gtype in gate.py')
+			if(gtype=='nand'):
+				res = not res
+			j[-1] = (1.0 if (res==j[-2]) else 0.0)
+		return table
+
+
+
+
+
+
+
+
+
+
