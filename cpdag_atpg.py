6ad019b22895166ef82d0916ee2d8ceefce89730
鄭景鴻 <zhengjinghong@zhengjinghongde-MacBook-Pro.local>
2019-05-28 14:25:51 +0800

    write file

--- cpdag_atpg.py
+++ cpdag_atpg.py
@@ -33,7 +33,9 @@ class CPD:
 	def parsar(self):
 		'''
 		parasar file
-
+		because the 
+		condatitional probability come from the gate 
+		our wire mean the ouput of gate 
 		'''
 		num=0
 		infile=open(self.filename,'r')
@@ -42,6 +44,7 @@ class CPD:
 			inp = line.strip()
 			inp = line.split()
 
+			# inilization wire name
 			if line[0]=='i' :
 				w1 = Gate(deepcopy(inp[0]),deepcopy(inp[1:]),self.model,deepcopy(num), self.Gate_dict, self.exe_set, self.gmt)
 				self.Gate_dict.setdefault(w1.return_name(),w1)
@@ -62,11 +65,14 @@ class CPD:
 
 	def logic_simulation(self):
 		# print(self.Gate_dict)
+		# set input wire
 		print('[parsar]\n ..Logic simulation' )
 		for i in self.input_wire_set:
 			self.Gate_dict[i].bay_conditional_prob_buid()
-
-
+		'''
+		set wire form bottom to top
+		to satisfy the mechanism of pomegrante
+		'''
 		for i in self.Gate_dict:
 			if(i in self.exe_set):
 				pass
@@ -80,17 +86,31 @@ class CPD:
 
 
 		# print(self.input_wire_set)
-		# print(self.gmt)
+		print(self.gmt)
 		# print('')
 		# self.model.plot(self.filename)
-		print(self.model.predict_proba({}))
+		# 
 		# print(self.model.predict_proba({self.output_wire[1]:True}))
 
-	
-
-
-
-
+	def write_iniline_probability_table(self, filename='output.txt'):
+		ofile = open(filename, 'w')
+		nlist = (self.model.predict_proba({}))
+		for i in range(0,len(self.gmt)):
+			ofile.write(self.gmt[i]+'\n')
+			
+			t=(' '.join((str(nlist[i]).split(':'))) )
+			t=(' '.join((str(nlist[i]).split(','))) )
+			words= t.split()
+			for i in range(0,len(words)):
+				if words[i]=='"True"':
+					ofile.write(('True '+ words[i+1]))
+					break	
+			for i in range(0,len(words)):
+				if words[i]=='"False"':
+					ofile.write(('\nFalse '+ words[i+1] + '\n\n'))
+					break
+			# ofile.write(str(nlist[i])+'\n' )
+		ofile.close()
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import sys
+import time
 from cpdag_atpg import *
-
-
-
-
-
 
 
 
@@ -27,12 +23,22 @@ def main():
 		# cpd.return_bayRef().plot("Circuit")
 		cpd.logic_simulation()
 		print('[Operation]\n ..Probability operation')
+
+		start = time.time()
+
+		nlist = (cpd.return_bayRef().predict_proba({}))
+		# for i in range(0,len(nlist)):
+			# print(nlist[i]['parameters'])
+		end = time.time()
+		print(' ..predict_proba run time:',(end-start),'seconds' )
 		# cpd.return_bayRef().predict({'545GAT(287)':True})
 		# print(len(cpd.return_bayRef().predict_proba({'22GAT(10)':True})))
-		# print(cpd.return_bayRef().predict_proba({'22GAT(10)':True}))
+		# print(cpd.return_bayRef().predict_proba({}))
 		# cpd.return_bayRef().plot(argv[2])
 		# print(cpd.return_bayRef().predict_proba({"1GAT(0)":True}))
 
+		print(" ..Write file")
+		cpd.write_iniline_probability_table( str(argv[1]+'.out'))
 		print("[main] program end")
 	# except:
 	# 	print("[Wrong!!] ")
