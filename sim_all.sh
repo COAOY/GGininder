1020c03f386c25222cb383c58d12db57bc8ed9e3
xeniacjen <jonah0604@gmail.com>
2019-06-06 11:58:07 +0800

    data

--- sim_all.sh
+++ sim_all.sh
@@ -8,19 +8,19 @@ do
 	filename=log${arr[$i]}.txt
     touch $filename
     echo ${arr[$i]}
-    python main.py  $(pwd)/sample_circuits/bseries/b${arr[i]}_C.bench.txt.ckt > $filename
+    python main.py  $(pwd)/sample_circuits/bseries/b${arr[i]}_C.bench.txt.ckt # > $filename
 
-    echo " " >> $filename
-    echo " " >> $filename
-    echo "[Level selection]" >> $filename
+    echo " " # >> $filename
+    echo " " # >> $filename
+    echo "[Level selection]" #>> $filename
 	time $(pwd)/src/./atpg -tdfatpg -ndet 1 $(pwd)/sample_circuits/bseries/b${arr[i]}_C.bench.txt.ckt > ../log1
 	python $(pwd)/src/util.py ../log1 >> $filename
 
-	echo " " >> $filename
-    echo " " >> $filename
-	echo "[CPDAG selection]" >> $filename
+	echo " " # >> $filename
+    echo " " #>> $filename
+	echo "[CPDAG selection]" #>> $filename
 	time $(pwd)/src/./atpg -tdfatpg -cpdag -ndet 1 $(pwd)/sample_circuits/bseries/b${arr[i]}_C.bench.txt.ckt > ../log2
-	python $(pwd)/src/util.py ../log2 >> $filename
+	python $(pwd)/src/util.py ../log2 #>> $filename
    
 
        
